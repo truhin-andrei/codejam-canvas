752944ae97cd83eaf069d8aad9e3d2e284eef980
truhin-andrei <truhin-andrei@bk.ru>
2019-10-27 13:55:48 +0700

    feat: add 32x32

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,8 @@ let canvas  = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let matrix;
 let four = document.getElementById('four');
+let thirty_two = document.getElementById('thirty_two');
+
 four.addEventListener('click', () =>
 fetch('./assets/json/4x4.json')
   .then(function(response) {  
@@ -23,5 +25,25 @@ fetch('./assets/json/4x4.json')
     });  
   }));
 
+  thirty_two.addEventListener('click', () =>
+fetch('./assets/json/32x32.json')
+  .then(function(response) {  
+    return response.json();  
+  })  
+  .then(function(matrix) {
+    let dx = 0;
+    let dy = 0;
+    let width = canvas.width/matrix[0].length;
+    let height = canvas.height/matrix.length;
+    matrix.forEach((row,index) =>{
+      if (dx != 0) dy += height;
+       dx =0;
+      row.forEach((col, k) => {
+        context.fillStyle =`rgba(${col[0]}, ${col[1]}, ${col[2]}, ${col[3]})`;
+        context.fillRect(dx, dy, width, height);
+        dx += width;  
+      });
+    });  
+  }));
   
 
